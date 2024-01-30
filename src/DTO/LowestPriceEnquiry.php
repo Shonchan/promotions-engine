a8d536eed947b77493730e4f1db70290b98a5051
Alex Shaforostov <czero57@gmail.com>
2024-01-30 15:05:25 +0500

    add DTO classes and serializer

---
+++ src/DTO/LowestPriceEnquiry.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\DTO;
+
+class LowestPriceEnquiry
+{
+    private ?int $quantity = 1;
+
+    private ?string $requestLocation;
+
+    private ?string $voucherCode;
+
+    private ?string $requestDate;
+
+    private ?float $price;
+
+    /**
+     * @return int|null
+     */
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param int|null $quantity
+     */
+    public function setQuantity(?int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRequestLocation(): ?string
+    {
+        return $this->requestLocation;
+    }
+
+    /**
+     * @param string|null $requestLocation
+     */
+    public function setRequestLocation(?string $requestLocation): void
+    {
+        $this->requestLocation = $requestLocation;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getVoucherCode(): ?string
+    {
+        return $this->voucherCode;
+    }
+
+    /**
+     * @param string|null $voucherCode
+     */
+    public function setVoucherCode(?string $voucherCode): void
+    {
+        $this->voucherCode = $voucherCode;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRequestDate(): ?string
+    {
+        return $this->requestDate;
+    }
+
+    /**
+     * @param string|null $requestDate
+     */
+    public function setRequestDate(?string $requestDate): void
+    {
+        $this->requestDate = $requestDate;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float|null $price
+     */
+    public function setPrice(?float $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getDiscountedPrice(): ?float
+    {
+        return $this->discountedPrice;
+    }
+
+    /**
+     * @param float|null $discountedPrice
+     */
+    public function setDiscountedPrice(?float $discountedPrice): void
+    {
+        $this->discountedPrice = $discountedPrice;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPromotionId(): ?int
+    {
+        return $this->promotionId;
+    }
+
+    /**
+     * @param int|null $promotionId
+     */
+    public function setPromotionId(?int $promotionId): void
+    {
+        $this->promotionId = $promotionId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPromotionName(): ?string
+    {
+        return $this->promotionName;
+    }
+
+    /**
+     * @param string|null $promotionName
+     */
+    public function setPromotionName(?string $promotionName): void
+    {
+        $this->promotionName = $promotionName;
+    }
+
+    private ?float $discountedPrice;
+
+    private ?int $promotionId;
+
+    private ?string $promotionName;
+}
