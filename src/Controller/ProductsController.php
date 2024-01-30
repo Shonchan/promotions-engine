bda6ed35ff2f2a95b178a3f4fe78a46279bf3b9f
Alex Shaforostov <czero57@gmail.com>
2024-01-30 14:31:38 +0500

    routes dummy

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,39 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ProductsController
 {
+    #[Route('/products/{id}/lowest-price', name:'lowest-price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id): Response
+    {
+        if($request->headers->has('force-fail')) {
+            return  new JsonResponse(
+                ['error' => 'Promotions Engine Error'], $request->headers->get('force-fail'));
+        }
 
+        return  new JsonResponse([
+            "quantity" => 5,
+            "request_location" => "UK",
+            "voucher_code" => "OU0812",
+            "request_date" => date("Y-m-d"),
+            "product_id" => $id,
+            "price" => 100,
+            'discounted_price' => 50,
+            'promotion_id' => 3,
+            'promotion_name' => 'Black Friday half price sale'
+        ], 200);
+    }
+
+
+
+    #[Route('/products/{id}/promotions', name:'promotions', methods: 'GET')]
+    public function promotions()
+    {
+        
+    }
 }
